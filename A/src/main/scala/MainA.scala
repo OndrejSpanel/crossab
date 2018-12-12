e90dc35be95e8ced5006db66977d1aabc3c6f7b4
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2018-12-12 10:13:57 +0100

    Add 2.11 / 2.12 specific functionality.

--- A/src/main/scala/MainA.scala
+++ A/src/main/scala/MainA.scala
@@ -1,3 +1,10 @@
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object MainA extends App{
+  val f  = Future {}
+  // Scala 2.11 specific code
+  f.transform(ok => ok, err => err)
+  println("I am A")
   Common.common()
 }
--- B/src/main/scala/MainB.scala
+++ B/src/main/scala/MainB.scala
@@ -1,3 +1,10 @@
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object MainB extends App{
+  val f  = Future {}
+  // Scala 2.12 specific code
+  f.transform(res => res)
+  println("I am B")
   Common.common()
 }
